3babb545e7d073c1137230a14d84da9e6dd1264a
Evan Robinson <34613006+Erobtech@users.noreply.github.com>
2019-05-14 18:55:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var e1 = document.querySelector('.toggle');
+
+e1.onclick = function() {
+  el.classList.toggle('active');
+
 function test() {
   var x = document.getElementById("test");
   x.innerHTML = "Hopefully this will change.";
